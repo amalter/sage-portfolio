b0308ea2a6810c6c87100fbd411239e2b7f3446c
Amber Alter <aarnold@Ambers-Air.domain>
2020-01-05 13:33:28 -0800

    Add function to loop through custom portfolio_topics custom post type

--- app/Controllers/FrontPage.php
+++ app/Controllers/FrontPage.php
@@ -6,5 +6,22 @@ use Sober\Controller\Controller;
 
 class FrontPage extends Controller
 {
-
+    //Loop portfolio CPT
+    public function portfolioLoop()
+    {
+        $portfolio_topics = get_posts([
+        'post_type' => 'portfolio_topic',
+        'posts_per_page'=>'3',
+        'order' => 'ASC',
+        ]);
+        
+        return array_map(function ($post) {
+            return [
+            'content' => apply_filters('the_content', $post->post_content),
+            'thumbnail' => get_the_post_thumbnail($post->ID, 'large'),
+            'thumbnail_url' => get_the_post_thumbnail_url($post->ID, 'large'),
+            'title' => $post->post_title,
+            ];
+        }, $portfolio_topics);
+    }
 }
--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -4,4 +4,11 @@
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-page')
   @endwhile
+<section class="front-page_section front-page_section-portfolio" id="portfolio">
+    @foreach($portfolio_loop as $portfolio_topic)
+    <div class="front-page_section-portfolio_topic" style="background: linear-gradient( rgba(38, 36, 35, 0.3), rgba(38, 36, 35, 0.3) ), url({!! $portfolio_topic['thumbnail_url'] !!});">
+      <h2 class="front-page_section-portfolio_topic-title">{!! $portfolio_topic['title'] !!}</h2>
+    </div>  
+    @endforeach
+</section><!--#portfolio-->
 @endsection
