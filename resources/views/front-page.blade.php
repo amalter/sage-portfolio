258f6c4bc2d9f390d1a5604d8ffc49ca74f08e39
Amber Alter <aarnold@Ambers-MacBook-Air.local>
2020-08-25 14:56:06 -0700

    change layout

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -3,16 +3,23 @@
 @section('content')
 
 <section class="front-page_section front-page_section-portfolio" id="portfolio">
+  <h2 class="front-page_section-title front-page_section-portfolio-title">Portfolio</h2>
+  <div class="front-page_section-portfolio_topics">
     @foreach($portfolio_loop as $portfolio_topic)
-    <div class="front-page_section-portfolio_topic" style="background: linear-gradient( rgba(38, 36, 35, 0.3), rgba(38, 36, 35, 0.3) ), url({!! $portfolio_topic['thumbnail_url'] !!});">
-      <h2 class="front-page_section-portfolio_topic-title">{!! $portfolio_topic['title'] !!}</h2>
+    <div class="front-page_section-portfolio_topics--topic" style="background: linear-gradient( rgba(38, 36, 35, 0.3), rgba(38, 36, 35, 0.3) ), url({!! $portfolio_topic['thumbnail_url'] !!});">
+      <h2 class="front-page_section-portfolio_topics--topic-title">{!! $portfolio_topic['title'] !!}</h2>
+    {{-- <div>{!! $portfolio_topic['content'] !!}</div> --}}
     </div>  
     @endforeach
+  </div>
+
 </section><!--#portfolio-->
-<section class="front-page_section" id="about-me">
-about me
+<section class="front-page_section front-page_section-about-me" id="about-me">
+  <h2 class="front-page_section-title front-page_section-about-me-title">About Me</h2>
+
 </section><!--#about-me-->
-<section class="front-page_section" id="contact">
+<section class="front-page_section front-page_section-contact" id="contact">
+  <h2 class="front-page_section-title front-page_section-contact-title">Contact</h2>
 contact
 </section><!--#contact-->
 @endsection
