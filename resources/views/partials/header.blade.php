e4d912ce7246e7798dbc85da33e544b7cbe2f816
Amber Alter <aarnold@Ambers-MacBook-Air.local>
2020-09-03 16:43:42 -0700

    set up site header and styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,9 @@
   <div class="container">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
+      {{-- @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
+      @endif --}}
     </nav>
   </div>
 </header>
