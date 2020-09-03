0ace108905065e0d411af1bbe9f66c3374cf98d4
Amber Alter <aarnold@Ambers-MacBook-Air.local>
2020-09-03 16:01:37 -0700

    set up site footer and add styles

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,10 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+<footer class="footer">
+  <div class="footer_container">
+    <div class="copyright"> &copy;2014–@php echo date('Y');@endphp | <a href="{{ home_url('/') }}">Amber Alter</a></div>
+    <div class="social-icons">
+      <a href="https://www.linkedin.com/in/amberalter/"><img src="@asset('images/linkedin.png')" alt="linkedin logo"></a>
+      <a href="https://github.com/amalter?tab=repositories"><img src="@asset('images/github.png')" alt="githublogo"></a>
+    </div>
+   
   </div>
 </footer>
